14a14ce244a95957f7a095c83d0528a366b74eaa
smritter34 <135673027+smritter34@users.noreply.github.com>
2023-12-05 08:16:48 -0600

    finalpush

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "739d2fc8780a7835d7ec6dc0ec3af3fe"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a04d42767e8443b0884be8da00e6186a"
